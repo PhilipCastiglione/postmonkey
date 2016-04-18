95bd20c7492e8472defcc04a09b9ae5dda22409b
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-04-18 15:39:30 +1000

    bundled gibbon and added openssl verification skip

--- postmonkey.rb
+++ postmonkey.rb
@@ -10,8 +10,12 @@ Dotenv.load
 
 MAILCHIMP_API_KEY = ENV['MAILCHIMP_API_KEY']
 
-Excon.defaults[:ssl_verify_peer] = false # this is an unsafe thing I am doing to be lazy and let these API calls work in dev without proper SSL cert chain
+# these lines are unsafe and shouldn't be used in prod - I'm just disabling ssl verification
+# as mailchimp has cert issues currently; refer https://github.com/amro/gibbon/issues/173
+Excon.defaults[:ssl_verify_peer] = false
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 mailchimp = Mailchimp::API.new(MAILCHIMP_API_KEY)
-gibbon = Gibbon::Request.new(api_key: MAILCHIMP_API_KEY)
+#gibbon = Gibbon::Request.new(api_key: MAILCHIMP_API_KEY) # for example only
+
 binding.pry
